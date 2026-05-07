d7cf55c8507c2c1000b5073bd426a71022289709
Abhay Aysola <abhay.ayyasola@gmail.com>
2026-05-08 00:01:00 +0530

    car no go vroom

--- src/nova_ftg/nova_ftg/ftg_node.py
+++ src/nova_ftg/nova_ftg/ftg_node.py
@@ -7,7 +7,7 @@ from ackermann_msgs.msg import AckermannDrive
 class GapFollower(Node):
     def __init__(self):
         super().__init__('gap_follower_node')
-        self.ftg = FTG_Controller(mapping=False, debug=True, safety_radius=5, max_lidar_dist=10, max_speed=20, range_offset=270, track_width=4)
+        self.ftg = FTG_Controller(mapping=False, debug=True, safety_radius=5, max_lidar_dist=10, max_speed=5, range_offset=270, track_width=4)
         
         # Subscriber to the LiDAR topic
         self.lidar_sub = self.create_subscription(
